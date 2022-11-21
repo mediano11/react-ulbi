af2225f8abff75b705b8b2a796b61cc323079cf1
pest2811@gmail.com <pest2811@gmail.com>
2022-11-21 13:58:04 +0200

    add fetch method & loader & decompose sorting posst

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 import "./styles/App.css"
-import PostItem from "./components/PostItem";
-import {useMemo, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
-import MySelect from "./components/UI/select/MySelect";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
+import {usePosts} from "./components/hooks/usePost";
+import PostService from "./components/API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id:1, title: "ааа", body: "бб"},
-    {id:2, title: "бб", body: "вв"},
-    {id:3, title: "гг", body: "гг"},
-    {id:4, title: "дд", body: "аа"}
-  ])
+  const [posts, setPosts] = useState([])
 
     const [filter,setFilter] = useState({sort:"", query:""})
-
-  const sortedPosts = useMemo(()=>{
-      if(filter.sort){
-          return [...posts].sort((a,b)=>a[filter.sort].localeCompare(b[filter.sort]))
-      }
-      return posts
-  }, [filter.sort, posts]);
-  const sortedAnSearchedPosts = useMemo(()=>{
-      return sortedPosts.filter(post=> post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
-
+    const [modal, setModal] = useState(false)
+    const [isPostLoading, setIsPostLoading] = useState(false)
+    const sortedAnSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
     const createPost = (newPost) =>{
         setPosts([...posts, newPost]);
@@ -37,7 +24,15 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(el=>el.id !== post.id))
     }
-    const [modal, setModal] = useState(false)
+    async function fetchPosts() {
+        setIsPostLoading(true)
+        const posts = await PostService.getAll();
+        setPosts(posts);
+        setIsPostLoading(false)
+    }
+    useEffect(()=>{
+        fetchPosts()
+    }, [])
   return (
     <div className="App">
         <MyButton style={{marginTop:"15px"}}onClick={()=>setModal(true)}>
@@ -48,7 +43,10 @@ function App() {
         </MyModal>
       <hr style={{margin: "15px 0"}}/>
         <PostFilter filter={filter} setFilter={setFilter}/>
-        <PostList remove={removePost} posts={sortedAnSearchedPosts} title="JS list"/>
+        {isPostLoading
+            ? <div style={{display: "flex", justifyContent:"center", marginTop: "50px"}}><Loader/></div>
+            : <PostList remove={removePost} posts={sortedAnSearchedPosts} title="JS list"/>
+        }
     </div>
   );
 }
---
+++ src/components/UI/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import classes from "./Loader.module.css";
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div className={classes.loader}>
+
+        </div>
+    );
+};
+
+export default Loader;
